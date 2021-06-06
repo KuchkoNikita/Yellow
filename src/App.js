804beb855b13f057fe679eb70a49edd2b35e47a5
Nikita Kuchko <kuchko.n.del@gmail.com>
2021-06-06 22:57:57 +0300

    Add react-router-dom in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Card from './components/Cards/index';
+import Form from './components/Form/index';
+import Info from './components/Info/index';
+import LogIn from './components/LogIn/index';
+import Empty from './components/Empty/index';
+import Layout from './hoc/Layout';
 
 function App() {
-  return <div />;
+  const routers = (
+    <Switch>
+      <Route path={'/empty'} component={Empty} />
+      <Route path={'/cards'} component={Card} />
+      <Route path={'/jogs'} component={Form} />
+      <Route path={'/contacts'} component={LogIn} />
+      <Route path={'/info'} exact component={Info} />
+      <Redirect to="/jogs" />
+    </Switch>
+  );
+
+  return (
+    <BrowserRouter>
+      <Layout>{routers}</Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
