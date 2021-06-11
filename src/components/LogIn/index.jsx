c6d39e4dbd0e977dbe01eb487cc83e21a6e3e3b0
Nikita Kuchko <kuchko.n.del@gmail.com>
2021-06-11 17:55:54 +0300

    Added redax to login component

--- src/components/LogIn/index.jsx
+++ src/components/LogIn/index.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import { ReactComponent as BearSvg } from './../../assets/bear-face.svg';
+import { auth } from '../../store/actions/auth';
 import './style.scss';
 import './../../styles/common.scss';
-import { ReactComponent as BearSvg } from './../../assets/bear-face.svg';
 
-const LogIn = () => {
+const LogIn = ({ auth }) => {
+  const history = useHistory();
+  const handleClick = () => {
+    auth();
+    history.push('/jogs');
+  };
+
   return (
-    <div className="login">
-      <BearSvg className="login__image" />
-      <button className="button login__button">Let me in</button>
+    <div className="inner">
+      <div className="login">
+        <BearSvg className="login__image" />
+        <button onClick={handleClick} className="button login__button">
+          Let me in
+        </button>
+      </div>
     </div>
   );
 };
 
-export default LogIn;
+LogIn.propTypes = {
+  auth: PropTypes.func,
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    auth: () => dispatch(auth()),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(LogIn);
