60bf0730945f21af764480083e751304b66c4e72
Nikita Kuchko <kuchko.n.del@gmail.com>
2021-06-11 17:53:44 +0300

    Centering the block

--- src/components/Empty/index.jsx
+++ src/components/Empty/index.jsx
@@ -5,10 +5,12 @@ import { ReactComponent as BearSvg } from './../../assets/sad-emoticon.svg';
 
 const LogIn = () => {
   return (
-    <div className="empty">
-      <BearSvg className="empty__image" />
-      <span className="empty__description">Nothing is there</span>
-      <button className="button empty__button">Create your jog first</button>
+    <div className="inner">
+      <div className="empty">
+        <BearSvg className="empty__image" />
+        <span className="empty__description">Nothing is there</span>
+        <button className="button empty__button">Create your jog first</button>
+      </div>
     </div>
   );
 };
