2a78e17e09452d1f340cde88bff072e6ba623791
Nikita Kuchko <kuchko.n.del@gmail.com>
2021-06-11 17:53:19 +0300

    We connect redax to the component Cards

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,18 +1,61 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import moment from 'moment';
+
+import { toDate } from './../../utils';
+
 import Card from './Card/Card';
+import { ReactComponent as AddSvg } from './../../assets/add.svg';
+
 import './style.scss';
 import './../../styles/common.scss';
 
-const Cards = () => {
+const Cards = ({ setIsOpenForm, isOpenFilter, cards, filterStart, filterEnd }) => {
+  const handleClick = () => setIsOpenForm(true);
+
+  const filterCards = isOpenFilter
+    ? useMemo(
+        () =>
+          cards.filter((card) =>
+            moment(toDate(card.date), 'DD.MM.YYYY').isBetween(filterStart, filterEnd)
+          ),
+        [cards, filterStart, filterEnd]
+      )
+    : cards;
+
   return (
     <>
       <div className="cards">
-        <Card />
-        <Card />
-        <Card />
+        {filterCards.map(({ id, distance, time, date }) => {
+          return (
+            <Card
+              key={id}
+              distance={distance}
+              time={time}
+              date={moment(date).format('DD.MM.YYYY')}
+              speed={Math.floor(distance / (time / 60))}
+            />
+          );
+        })}
       </div>
+      <AddSvg onClick={handleClick} className="add-icon" />
     </>
   );
 };
 
-export default Cards;
+Cards.propTypes = {
+  isOpenFilter: PropTypes.bool,
+  filterStart: PropTypes.object,
+  filterEnd: PropTypes.object,
+  cards: PropTypes.array,
+  setIsOpenForm: PropTypes.func,
+};
+
+const mapStateToProps = (state) => ({
+  isOpenFilter: state.filter.isOpenFilter,
+  filterStart: state.filter.filterStart,
+  filterEnd: state.filter.filterEnd,
+});
+
+export default connect(mapStateToProps)(Cards);
