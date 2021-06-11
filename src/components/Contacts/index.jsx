069ee4eae16d3f7e0884f2194d629f18ddc4489f
Nikita Kuchko <kuchko.n.del@gmail.com>
2021-06-11 17:48:24 +0300

    Create contacts component

---
+++ src/components/Contacts/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './../../styles/common.scss';
+
+const Contacts = () => {
+  return <h1 className="inner">Контакты</h1>;
+};
+
+Contacts.propTypes = {};
+
+export default Contacts;
